2287c9f8b221b1c547f5ee2efee236035587f73f
Lucas Souto <lucasantanasilva@gmail.com>
2023-09-04 23:28:28 -0300

    version 2.1

---
+++ operadores/src/operadores.java
@@ -0,0 +1,130 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //classe Operadores.java
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        //classe Operadores.java
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+                
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        concatenacao = 1+"1"+1+1;
+
+        concatenacao = 1+"1"+1+"1";
+
+        concatenacao = "1"+1+1+1;
+
+        concatenacao = "1"+(1+1+1);
+
+        //classe Operadores.java
+        int numero = 5;
+                
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+
+        // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(valor);
+
+
+        // ComparacaoAvancada.java
+        public static void main(String[] args) {
+                
+                int numero1 = 130;
+                int numero2 = 130;
+                System.out.println(numero1 == numero2); //true
+                
+                Integer numero1 = 130;
+                Integer numero2 = 130;
+
+                System.out.println(numero1 == numero2); //false
+                
+                // A razão pela qual o resultado é false, é devido o Java tratar os valores
+                // Como objetos a partir de agora.
+                // Qual a solução ?
+                // Quando queremos comparar objetos, usamos o método equals
+                
+                System.out.println(numero1.equals(numero2)); 
+        }
+
+        // Operadores.java
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
+    
+        // ComparacaoAvancada.java
+        int numero1 = 1;
+        int numero2 = 1;
+
+        if(numero1== 2 & numero2 ++ == 2 )
+            System.out.println("As 2 condições são verdadeiras");
+
+        System.out.println("O numero 1 agora é " + numero1);
+        System.out.println("O numero 2 agora é " + numero2);
+
+        // Vamos mudar a linha 5 do código acima para: if(numero1== 2 && numero2 ++ == 2 )
+        
+    }
+}
