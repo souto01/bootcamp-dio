2c15551f70c3f055a399eb151d609723e779e00d
Lucas Souto <lucasantanasilva@gmail.com>
2023-09-05 00:37:19 -0300

    version-1.0

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,27 @@
+public class SmartTv {
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar (){
+        ligada = true;
+    }
+    
+    public void desligar (){
+        ligada = false;
+    }
+
+     public void aumentarVolume (){
+        volume ++;
+    }
+
+    public void abaixarVolume (){
+        volume --;
+    }
+
+    public void mudarCanal (){
+        canal += 1;
+    }
+
+
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println( " TV Ligada? "  + smartTv.ligada);
+        System.out.println( " Canal Atual: " + smartTv.canal);
+        System.out.println( " Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println( " Novo Status - " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println( " Novo Status - " + smartTv.ligada);
+        
+        smartTv.aumentarVolume();
+        System.out.println( " Novo Volume - " + smartTv.volume);
+
+        smartTv.abaixarVolume();
+        System.out.println( " Novo Volume - " + smartTv.volume);
+
+        smartTv.mudarCanal();
+        System.out.println( " Novo Canal - " + smartTv.canal);
+
+
+    }
+}
